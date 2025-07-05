6abf4c29b8d2735fc477dd599b984c41c97036c6
Riley Thompson <rjthompson2@dons.usfca.edu>
2025-07-05 14:20:16 -0400

    basic template for apify

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-# Code here
+import json
+from apify_client import ApifyClient
+
+# Input token here
+client = ApifyClient(token='')
+
+def is_on_apify():
+    return 'APIFY_IS_AT_HOME' in environ
+
+# Push the solution to the dataset
+def set_output(data):
+    if not is_on_apify():
+        with open('./apify_storage/datasets/default/solution.json', 'w') as output:
+            return output.write(json.dumps(data, indent=2))
+
+    dataset = client.dataset(environ.get('APIFY_DEFAULT_DATASET_ID'))
+    dataset.push_items([data])
+
+# Your code here
